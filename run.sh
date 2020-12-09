34d95b4798b0404ec4db7c0e1130d6218ddbf7af
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2020-12-09 14:05:46 +0100

    process alpahbetically

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -exec /home/mathieu/Perso/scripts/nfofix {} \;
+#!/bin/sh -e
+
+# sort alphabetically since vera connect on panasonic sort item by date (=creation time), thus need to process in the same order.
+set -x
+cd /media/freebox/Vidéos/repo/dessin_animes
+#find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -exec /home/mathieu/Perso/scripts/nfofix {} \;
+find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -print0 | sort -z | xargs -I{} -r0 /home/mathieu/Perso/scripts/nfofix {}
