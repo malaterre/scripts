84dbdbe64f9da9a928287803979f3818050c5325
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2020-12-10 13:30:03 +0100

    fixup

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # sort alphabetically since vera connect on panasonic sort item by date (=creation time), thus need to process in the same order.
-set -x
+# set -x
 cd /media/freebox/Vidéos/repo/dessin_animes
 #find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -exec /home/mathieu/Perso/scripts/nfofix {} \;
-find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -print0 | sort -z | xargs -I{} -r0 /home/mathieu/Perso/scripts/nfofix {}
+find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -print0 | sort -z | xargs -I{} -r0 /home/mathieu/Perso/scripts/nfofix --output /media/freebox/Vidéos/repo_new {}
