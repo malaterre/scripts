50fc2d253a4aeecd64fed1863ef3bbfdd93d59c9
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2020-12-15 11:44:07 +0100

    more fixes

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -e
-
-# sort alphabetically since vera connect on panasonic sort item by date (=creation time), thus need to process in the same order.
 # set -x
+
+# get latest ffmpeg
+PATH=$HOME/bin:$PATH
+
 cd /media/freebox/Vidéos/repo/dessin_animes
-#find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -exec /home/mathieu/Perso/scripts/nfofix {} \;
-find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -print0 | sort -z | xargs -I{} -r0 /home/mathieu/Perso/scripts/nfofix --output /media/freebox/Vidéos/repo_new {}
+
+#FIXME need somehow to sort alphabetically since vera connect on panasonic sort item by date (=creation time), thus need to process in the same order.
+#find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -print0 | sort -z | xargs -I{} -r0 /home/mathieu/Perso/scripts/nfofix --output /media/freebox/Vidéos/repo_new {}
+
+find . -type f \( -name '*.avi' -or -name '*.mkv' -or -name '*.mp4' \) -exec /home/mathieu/Perso/scripts/nfofix --output /media/freebox/Vidéos/repo_new {} \;
