7c2ca5ceba9e671657252a8920afa669f3f5f99a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2021-01-12 13:33:09 +0100

    cleanup

--- new.py
+++ new.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import requests
 import os
 import json
@@ -7,6 +8,7 @@ import json
 import bs4 as bs
 #from http import cookies
 import urllib.parse
+
 
 def extract_form_fields(soup):
     "Turn a BeautifulSoup form in to a dict of fields and default values"
@@ -19,7 +21,7 @@ def extract_form_fields(soup):
         # pff = pff.find(id='primary_facts_form'):
         # ignore submit/image with no name attribute
         if not 'type' in input:
-          continue
+            continue
         if input['type'] in ('submit', 'image') and not 'name' in input:
             continue
 
@@ -78,6 +80,7 @@ def extract_form_fields(soup):
 
     return fields
 
+
 def urlencode(s):
     return urllib.parse.quote(s)
 
@@ -90,48 +93,54 @@ def encode_prefs(prefs):
     return urlencode(bytes(json.dumps(prefs, separators=(',', ':')), 'utf-8'))
 
 
-url = 'https://www.themoviedb.org/movie/366644-little-door-gods/edit?active_nav_item=primary_facts'
-headers = requests.utils.default_headers()
-#rawdata = os.environ['TMDB_COOKIE']
-tmdb_session = os.environ['TMDB_SESSION']
-#headers['cookie'] = cookie
+def get_location(url):
+    response = requests.head(url, allow_redirects=True)
+    return response.url
 
-#headers['cookie'] = rawdata
-#cookie = cookies.SimpleCookie()
-# d=urldecode(headers['cookie'])
-#cookie = SimpleCookie()
-# cookie.load(rawdata)
-# cookie.load(rawdataold)
-cookies = {}
-# for key, morsel in cookie.items():
-#    cookies[key] = morsel.value
-# print(cookies)
-# print(headers)
-#tmdb_prefs = cookies['tmdb.prefs']
-# print(tmdb_prefs)
-#j = json.loads(urldecode(tmdb_prefs))
-#print(json.dumps(j, indent=4, sort_keys=True))
-tmdb_prefs = {"adult": True, "i18n_fallback_language": "en-US",
-              "locale": "fr-FR", "country_code": "FR", "timezone": "Europe/Paris"}
-# print(urlencode(json.dumps(tmdb_prefs2,separators=(',', ':')), 'utf-8')))
-# print(encode_prefs(tmdb_prefs))
-# cookies['tmdb.prefs'] = urlencode(json.dumps(tmdb_prefs2, separators=(',', ':')), 'utf-8'))
-cookies['tmdb.prefs'] = encode_prefs(tmdb_prefs)
-#del cookies['_ga']
-#del cookies['_gali']
-#del cookies['_gid']
-#del cookies['tmdb._cookie_policy']
-# del cookies['tmdb.session'] # write access !!
-cookies['tmdb.session'] = tmdb_session
-# del cookies['tmdb.prefs'] # default language (fr) !!
-#del cookies['_dc_gtm_UA-2087971-10']
-#print(json.dumps(cookies, indent=4, sort_keys=True))
-cookie_string = "; ".join([str(x)+"="+str(y) for x, y in cookies.items()])
-headers['cookie'] = cookie_string
-#del headers['cookie']
-r = requests.get(url, headers=headers)
-soup = bs.BeautifulSoup(r.content, 'html.parser')
-#print(soup.prettify())
-#print(soup.find(id='primary_facts_form'))
-ret = extract_form_fields(soup)
-print(ret)
+
+def main(args):
+    verbose = args.verbose
+    movie_id = args.movie_id
+    lang = args.lang
+
+    # url = 'https://www.themoviedb.org/movie/366644-little-door-gods/edit?active_nav_item=primary_facts'
+    root_url = 'https://www.themoviedb.org/movie/'
+    #    'referer': 'https://www.themoviedb.org/movie/9836-happy-feet/edit?active_nav_item=primary_facts',
+    url = get_location(root_url + str(movie_id)) + \
+        '/edit?active_nav_item=primary_facts'
+    if verbose:
+        print(referer)
+
+    headers = requests.utils.default_headers()
+    tmdb_session = os.environ['TMDB_SESSION']
+
+    cookies = {}
+    tmdb_prefs = {"adult": True, "i18n_fallback_language": "en-US",
+                  "locale": "fr-FR", "country_code": "FR", "timezone": "Europe/Paris"}
+    cookies['tmdb.prefs'] = encode_prefs(tmdb_prefs)
+    # cookies['tmdb.session'] # write access !!
+    cookies['tmdb.session'] = tmdb_session
+    # cookies['tmdb.prefs'] # default language (fr) !!
+    cookie_string = "; ".join([str(x)+"="+str(y) for x, y in cookies.items()])
+    headers['cookie'] = cookie_string
+    r = requests.get(url, headers=headers)
+    soup = bs.BeautifulSoup(r.content, 'html.parser')
+    # print(soup.prettify())
+    # print(soup.find(id='primary_facts_form'))
+    ret = extract_form_fields(soup)
+    print(ret)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="tmdb typo")
+    parser.add_argument("movie_id", help='movie id')
+    #
+    parser.add_argument("--dry-run",
+                        help="dry run", action='store_true')
+    # verbose
+    parser.add_argument("--lang",
+                        help="verbose output", action='store_true')
+    parser.add_argument("--verbose",
+                        help="verbose output", action='store_true')
+    main(parser.parse_args())
